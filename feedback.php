42c7d0cb94b29ab56155f572368ac93ce3bbde5f
meynard1623 <infantadojl@students.nu-baliwag.edu.ph>
2023-04-24 00:51:25 +0800

    First Commit

---
+++ feedback.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="uutf-8">
+<title>About</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <script src="https://kit.fontawsome.com/a076d05399.js"></script>
+<body>
+<nav>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+      <i class="fas fa-bars"></i>
+    </label>
+&nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; <img src="images/SheetsCristine.jpg" alt="My Image" style="width:60px;height:60px">
+    
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="services.html">Rates</a></li>
+      <li><a href="schedule.html">Schedule</a></li>
+      <li><a  href="about.html">About</a></li>
+      <li><a class="active" href="about.html">Feedback</a></li>
+    </ul>
+  </nav>
+
+<center> 
+  <img src="images/logo.jpg" alt="My Image" style="width:400px;height:360px">
+   <h1>Feedback Form</h1>
+   <form class="action="send_php" method="post">
+    
+   
+<br>
+<span class="details">Name</span>
+            <input type="text" placeholder=" " required> &nbsp;  &nbsp;
+
+            <span class="details">Email Address</span>
+            <input type="text" placeholder="" required>&nbsp;  &nbsp;
+        </center>
+        <br>
+            
+            <center> <span class="details">Comment</span> 
+              <br>
+            <textarea placeholder=""></textarea></div> &nbsp;  &nbsp; <br><br>
+
+<button style="background-color: lightblue; width:200px;">Submit</button> <br><br><br><br><br>
+</center> </form>
+
+<center> 
+<footer>&copy; SheetsCristine</footer>
+  
+</center>
+  </body>
+</html>   
